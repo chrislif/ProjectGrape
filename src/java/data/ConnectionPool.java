ea6eff7363d42554f07ad60bc804a81940865d41
Chris <chrislnk12@gmail.com>
2022-01-26 16:53:18 -0600

    database connection established

--- src/java/data/ConnectionPool.java
+++ src/java/data/ConnectionPool.java
@@ -1,13 +1,49 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package data;
 
-/**
- *
- * @author chris
- */
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.sql.DataSource;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
 public class ConnectionPool {
-    
+
+    private static ConnectionPool pool = null;
+    private static DataSource dataSource = null;
+    private static final Logger LOG = Logger.getLogger(ConnectionPool.class.getName());
+
+    private ConnectionPool() {
+        try {
+            InitialContext ic = new InitialContext();
+            //This links up with the resource tag in context.xml
+            dataSource = (DataSource) ic.lookup("java:/comp/env/jdbc/users");
+        } catch (NamingException e) {
+            LOG.log(Level.SEVERE, null, e);
+        }
+    }
+
+    public static synchronized ConnectionPool getInstance() {
+        if (pool == null) {
+            pool = new ConnectionPool();
+        }
+        return pool;
+    }
+
+    public Connection getConnection() {
+        try {
+            return dataSource.getConnection();
+        } catch (SQLException e) {
+            LOG.log(Level.SEVERE, null, e);
+            return null;
+        }
+    }
+
+    public void freeConnection(Connection c) {
+        try {
+            c.close();
+        } catch (SQLException e) {
+            LOG.log(Level.SEVERE, null, e);
+        }
+    }
 }
