bc39b14b910708537f2a2510891500333d6d30b3
Chris <chrislnk12@gmail.com>
2022-02-04 11:29:49 -0600

    updated sql file

--- web/resources/projectgrape.sql
+++ web/resources/projectgrape.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 26, 2022 at 10:36 PM
+-- Generation Time: Feb 04, 2022 at 06:26 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.12
 
@@ -29,10 +29,20 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `account` (
   `accountID` int(11) NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  `fullname` varchar(50) NOT NULL
+  `userName` varchar(20) NOT NULL,
+  `nickname` varchar(50) DEFAULT NULL,
+  `accountType` varchar(10) NOT NULL,
+  `salt` varchar(50) NOT NULL,
+  `hash` mediumtext NOT NULL,
+  `email` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `account`
+--
+
+INSERT INTO `account` (`accountID`, `userName`, `nickname`, `accountType`, `salt`, `hash`, `email`) VALUES
+(8, 'Chris', NULL, 'Student', 'J9WRSKNI8Z', 'E96655AED50C0994273204E4671B14CD74B2E2FD43ADCB8730705C6DC73B7B87', NULL);
 
 --
 -- Indexes for dumped tables
@@ -52,7 +62,7 @@ ALTER TABLE `account`
 -- AUTO_INCREMENT for table `account`
 --
 ALTER TABLE `account`
-  MODIFY `accountID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `accountID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
