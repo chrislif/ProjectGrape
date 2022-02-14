2479ff15c1bc69515acfcbfd10cbcc3569f58b22
dh687287 <dh687287@LR002D03.southeast.edu>
2022-02-14 13:57:14 -0600

    questiontype in js

--- web/page/assessments/assessment.js
+++ web/page/assessments/assessment.js
@@ -7,10 +7,12 @@ $(document).ready(() => {
 function startQuiz() {
     var questionLevel = $("#questionLevels").find(":selected").text();
     
+    var questionType = $("#questionType").find(":selected").text();
+    
     $.ajax({
         type: "POST",
         url: "private",
-        data: {'action' : 'generateQuiz', 'questionLevels' : questionLevel},
+        data: {'action' : 'generateQuiz', 'questionLevels' : questionLevel, 'questionType' : questionType},
         dataType: "JSON",
         success: function(result) {
             var questionList = JSON.parse(result);
@@ -31,18 +33,16 @@ function displayQuiz(questionList) {
     quiz.append('<div class="subContent"><ol id="quiz"> </ol></div>');
     
     for(let i in questionList) {
-        $('#quiz').append('<li>' + questionList[i].questionText + '</li>' + '<br>' +
-                "<input type=text id=question" + i + " class=quizQuestion>");
+        $('#quiz').append('<li>' + questionList[i].questionText + '<br>' +
+                "<input type=text id=question" + i + " class=quizQuestion>" + '</li>');
     }
     quiz.append("<input type='button' class='styledButton' id='endQuiz' onclick='endQuiz()' value='Submit Quiz!'>");
 }
 
 function endQuiz() {
     
-    var quizQuestions = $(".quizQuestion");
-    for (let question of quizQuestions) {
-        alert($(question.val()));
-    }
+    var quizQuestions = $("#quiz quizQuestion");
+    console.log(quizQuestions[0]);
     
 }
 
