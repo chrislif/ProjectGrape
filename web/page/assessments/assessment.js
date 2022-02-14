fc746059988e20b32eeec79375a4f25dcb79f280
dh687287 <dh687287@LR002D03.southeast.edu>
2022-02-14 15:14:58 -0600

    Submit quiz functionality

--- web/page/assessments/assessment.js
+++ web/page/assessments/assessment.js
@@ -1,8 +1,19 @@
 "use strict";
 
+var questionList;
+
 $(document).ready(() => {
+
     $("#startQuiz").click(startQuiz);
 });
+
+class Score {
+    constructor(userAnswer, correctAnswer) {
+        this.userAnswer = userAnswer;
+        this.correctAnswer = correctAnswer;
+        this.isCorrect = false;
+    }
+}
 
 function startQuiz() {
     var questionLevel = $("#questionLevels").find(":selected").text();
@@ -15,7 +26,7 @@ function startQuiz() {
         data: {'action' : 'generateQuiz', 'questionLevels' : questionLevel, 'questionType' : questionType},
         dataType: "JSON",
         success: function(result) {
-            var questionList = JSON.parse(result);
+            questionList = JSON.parse(result);
             
             displayQuiz(questionList);
         },
@@ -41,9 +52,31 @@ function displayQuiz(questionList) {
 
 function endQuiz() {
     
-    var quizQuestions = $("#quiz quizQuestion");
-    console.log(quizQuestions[0]);
+    var quizAnswerTexts = $("input[type='text']");
     
+    var scoreList = [];
+    
+    var answers = [];
+    
+    //Add Validation
+    
+    for (let i = 0; i < quizAnswerTexts.length; i++  ) {
+        answers.push(parseInt(quizAnswerTexts[i].value));
+    }
+
+    for (let i = 0; i <questionList.length; i++) {
+        var newScore = new Score(answers[i] ,parseInt(questionList[i].questionAnswer));
+        
+        if (answers[i] === parseInt(questionList[i].questionAnswer)) {
+           newScore.isCorrect = true;
+        } else {
+           newScore.isCorrect = false;
+        }
+        
+        scoreList.push(newScore);
+    }
+    
+    return scoreList;
 }
 
 function clearQuiz() {
