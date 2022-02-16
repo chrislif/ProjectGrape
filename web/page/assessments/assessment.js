c595e9f7dcd2bf6a372133aedcfcc74b8c812b85
dh687287 <dh687287@LR002D03.southeast.edu>
2022-02-16 13:58:44 -0600

    Display score functionality

--- web/page/assessments/assessment.js
+++ web/page/assessments/assessment.js
@@ -41,16 +41,18 @@ function displayQuiz(questionList) {
     
     var quiz = $("#quiz-container");
     
+    quiz.show();
+    
     quiz.append('<div class="subContent"><ol id="quiz"> </ol></div>');
     
     for(let i in questionList) {
         $('#quiz').append('<li>' + questionList[i].questionText + '<br>' +
-                "<input type=text id=question" + i + " class=quizQuestion>" + '</li>');
+                "<input type=text id=question" + i + ' ' +'</li>');
     }
-    quiz.append("<input type='button' class='styledButton' id='endQuiz' onclick='endQuiz()' value='Submit Quiz!'>");
+    quiz.append("<input type='button' class='styledButton' id='processQuiz' onclick='processQuiz()' value='Submit Quiz!'>");
 }
 
-function endQuiz() {
+function processQuiz() {
     
     var quizAnswerTexts = $("input[type='text']");
     
@@ -64,7 +66,7 @@ function endQuiz() {
         answers.push(parseInt(quizAnswerTexts[i].value));
     }
 
-    for (let i = 0; i <questionList.length; i++) {
+    for (let i = 0; i < questionList.length; i++) {
         var newScore = new Score(answers[i] ,parseInt(questionList[i].questionAnswer));
         
         if (answers[i] === parseInt(questionList[i].questionAnswer)) {
@@ -76,11 +78,32 @@ function endQuiz() {
         scoreList.push(newScore);
     }
     
+    displayScores(scoreList);
     return scoreList;
+}
+
+function displayScores(scoreList) {
+    
+    $("#processQuiz").hide();
+    
+     $("#quiz-container").append("<input type='button' class='styledButton' id='processQuiz' onclick='clearQuiz()' value='Clear Scores'>");
+    
+    for (let i in scoreList){
+        if (scoreList[i].isCorrect === true){
+            $("#question"+i).addClass("correctAnswer");
+        } else if (scoreList[i].isCorrect === false){
+            $("#question"+i).addClass("incorrectAnswer");
+        }
+        
+        $('#question'+i).before("<p> Correct Answer: " + scoreList[i].correctAnswer + "</p>");
+    }
 }
 
 function clearQuiz() {
     $("#quiz").empty();
+    $("#quiz-container").empty();
+    
+    $("#quiz-container").hide();
     
     $("#subContent").show();
 }
